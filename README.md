3cf6f1cbd0d1e02c0029efec5f41aae969bb6dbd
Davide <82649871+davidecarluccio@users.noreply.github.com>
2025-01-04 22:20:29 +0100

    Provided my solution

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# URL Shortener Microservice
+URL Shortener Microservice
+==========================
 
-This is the boilerplate code for the URL Shortener Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/url-shortener-microservice.
+Build a full stack JavaScript app that is functionally similar to this: [https://url-shortener-microservice.freecodecamp.rocks](https://url-shortener-microservice.freecodecamp.rocks). Working on this project will involve you writing your code using one of the following methods:
+
+*   Clone [this GitHub repo](https://github.com/freeCodeCamp/boilerplate-project-urlshortener/) and complete your project locally.
+*   Use [our Gitpod starter project](https://gitpod.io/?autostart=true#https://github.com/freeCodeCamp/boilerplate-project-urlshortener/) to complete your project. Learn [how to share your Gitpod workspace to get help](https://forum.freecodecamp.org/t/how-to-use-gitpod-in-the-curriculum/668669#how-can-i-share-my-workspace-to-get-help-8).
+*   Use a site builder of your choice to complete the project. Be sure to incorporate all the files from our GitHub repo.
+
+* * *
+
+**HINT:** Do not forget to use a body parsing middleware to handle the POST requests. Also, you can use the function `dns.lookup(host, cb)` from the `dns` core module to verify a submitted URL.
+
+Tests
+-----
+
+1\. You should provide your own project, not the example URL.
+    
+2\. You can POST a URL to `/api/shorturl` and get a JSON response with `original_url` and `short_url` properties. Here's an example: `{ original_url : 'https://freeCodeCamp.org', short_url : 1}`
+    
+3\. When you visit `/api/shorturl/<short_url>`, you will be redirected to the original URL.
+    
+4\. If you pass an invalid URL that doesn't follow the valid `http://www.example.com` format, the JSON response will contain `{ error: 'invalid url' }`
--- index.js
+++ index.js
@@ -1,24 +1,131 @@
 require('dotenv').config();
+// const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
+
+
 const app = express();
-
-// Basic Configuration
-const port = process.env.PORT || 3000;
-
 app.use(cors());
-
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.get('/', (_, res) => res.sendFile(process.cwd() + '/views/index.html'));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get('/api/hello', (_, res) => res.json({ greeting: 'hello API' }));
+
+// MONGO DB APPROACH
+/*
+  const clientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+  mongoose.connect(`${process.env.MONGO_PROTOCOL}${process.env.MONGO_USER}${process.env.MONGO_PW}${process.env.MONGO_URI}`, clientOptions);
+  const urlSchema = new mongoose.Schema({
+    original: { type: String, required: true },
+    short: Number
+  });
+  const URL = mongoose.model('url', urlSchema);
+*/
+
+// POST VIA MONGODB
+/*
+  app.post('/api/shorturl', bodyParser.urlencoded({ extended: false }), (req, res) => {
+    const inputURL = req.body.url;
+    let shortInput = 1;
+    
+    // matching by protocol, if not http/s or ws/s return error
+    const urlRegex = new RegExp(/^(https?|wss?):\/\/[^\s/$.?#].[^\s]*$/);
+    if (!inputURL.match(urlRegex)) {
+      res.json({ error: 'Invalid URL' });
+      return;
+    }
+
+    // query mongodb
+    URL.findOne({})
+      .sort({short: 'desc'})
+      .exec((error, result) => {
+        if(!error && result != undefined) {
+          shortInput = result.short + 1;
+        } 
+        if (!error) {
+          // update mongodb
+          URL.findOneAndUpdate(
+            {original: inputURL}, 
+            {original: inputURL, short: shortInput}, 
+            {new: true, upsert: true}, 
+            (err, record) => {
+              if (!err) {
+                res.json({ original_url: inputURL, short_url: record.short })
+              }
+            });
+        }
+      });
+  });
+*/
+
+// GET VIA MONGODB
+/*
+  app.get('/api/shorturl/:input', (req, res) => {
+    const input = req.params.input;
+    
+    URL.findOne({short: input}, (error, result) => {
+      // if input is found, redirect
+      if (!error && result !== undefined) {
+        res.redirect(result.original);
+        return;
+      } 
+      
+      // otherwise return error
+      res.json({ error: 'URL not found' });
+    })
+  });
+*/
+
+// IN-MEMORY APPROACH
+const memory = [
+  { original_url: "http://localhost:3000", short_url: 0 },
+];
+
+// POST VIA IN-MEMORY
+app.post('/api/shorturl', bodyParser.urlencoded({ extended: false }), (req, res) => {
+  const inputURL = req.body.url;
+
+  // matching by protocol, if not http/s or ws/s return error
+  const urlRegex = new RegExp(/^(https?|wss?):\/\/[^\s/$.?#].[^\s]*$/);
+  if (!inputURL.match(urlRegex)) {
+    res.json({ error: 'Invalid URL' });
+    return;
+  }
+
+  // if input matches an entry, return entry
+  const filterByName = memory.filter((entry) => entry.original === String(inputURL))
+  if (filterByName.length > 0) {
+    res.json(filterByName[0]);
+    return;
+  }
+  
+  // if input don't matches an entry, create new entry and return
+  if (filterByName.length === 0) {
+    const newEntry = { original_url: inputURL, short_url: memory.length }
+    memory.push(newEntry);
+    res.json(newEntry);
+    return;
+  }
+
+  // if anything other happens, return error mesage
+  res.json({ error: 'Invalid URL' });
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+// GET VIA IN-MEMORY
+app.get('/api/shorturl/:input', (req, res) => {
+  const input = req.params.input;
+
+  // if input matches a shorturl in memory, redirect to site
+  const filterByURL = memory.filter((entry) => entry.short_url === Number(input))
+  if (filterByURL.length > 0) {
+    res.redirect(filterByURL[0].original_url);
+    return;
+  }
+
+  // otherwise return error message
+  res.json({ error: "URL not found" });
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
